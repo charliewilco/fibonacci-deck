34dd7bf9af6f458f2ccb124c29a6dd865838a038
Charles ⚡ <charlespeters42@gmail.com>
2018-12-31 01:58:13 -0800

    now.json

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-// next.config.js
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === "development"
+    ? {}
+    : !process.env.NOW_REGION
+    ? require("next/constants")
+    : require("next-server/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+  // ✅ Put the require call here.
+
+  const withCSS = require("@zeit/next-css");
+  //
+
+  return withCSS();
+};
