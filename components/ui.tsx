2c6428cb71b0c4642a317bdcd3940f731b38a308
Charles ⚡ <charlespeters42@gmail.com>
2021-05-11 17:05:07 -0700

    tweaks

--- components/ui.tsx
+++ components/ui.tsx
@@ -37,7 +37,7 @@ export const Card = ({ datum }: CardProps) => {
   const onTap = () => setDisplay(datum.value);
   return (
     <button className="Card" onClick={onTap}>
-      {datum}
+      {datum.display}
     </button>
   );
 };
@@ -47,26 +47,26 @@ interface OpenCloseProps {
   onClick: (x: any) => void;
 }
 
-const DOWN_POLYLINE = "6 9 12 15 18 9";
-const UP_POLYLINE = "18 15 12 9 6 15";
-
-export const ArrowIcon = ({ open, onClick }: OpenCloseProps) => (
-  <button
-    className="Chevron"
-    onClick={onClick}
-    aria-label="Open or Close the Card Tray"
-  >
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+export const ArrowIcon = ({ open, onClick }: OpenCloseProps) => {
+  const points = open ? "6 9 12 15 18 9" : "18 15 12 9 6 15";
+  return (
+    <button
+      className="Chevron"
+      onClick={onClick}
+      aria-label="Open or Close the Card Tray"
     >
-      <polyline points={open ? DOWN_POLYLINE : UP_POLYLINE} />
-    </svg>
-  </button>
-);
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <polyline points={points} />
+      </svg>
+    </button>
+  );
+};
